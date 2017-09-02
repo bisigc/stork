45be441c356fb730564b664ac731e8be7d020691
Christian Bisig <cbisig@gmail.com>
2017-09-02 21:04:31 +0200

    Updated models.

--- stork/model/project.py
+++ stork/model/project.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Sequence
 from ../controller/dbController import Base
 
-class Note(Base):
-	__tablename__ = 'note'
-	id = Column(Integer, Sequence('note_id_seq'), primary_key=True)
-	note = Column(String(2500), nullable=True)
+class Project(Base):
+	__tablename__ = 'project'
+	id = Column(Integer, Sequence('project_id_seq'), primary_key=True)
+	title = Column(String(255), nullable=False)
+	description = Column(String(2500), nullable=True)
 	creator_id = Column(Integer(), nullable=False)
-	task_id = Column(Integer(), nullable=False)
 	created = Column(Float(), nullable=True)
 	modified = Column(Float(), nullable=True)
 	
---
+++ stork/model/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Sequence
+from ../controller/dbController import Base
+
+class User(Base):
+	__tablename__ = 'user'
+	id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+	username = Column(String(100), nullable=False)
+	password = Column(String(255), nullable=False)
+	firstname = Column(String(255), nullable=True)
+	surname = Column(String(255), nullable=True)
+	email = Column(String(255), nullable=False)
+	mobile = Column(String(50), nullable=True)
+	created = Column(Float(), nullable=True)
+	modified = Column(Float(), nullable=True)
+
